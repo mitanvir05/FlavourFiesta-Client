f7845b9da3eace336335d6fa95f27a2be4029860
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-26 15:16:29 +0600

    My cart functionality added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,18 +1,70 @@
+import { FaCartShopping } from "react-icons/fa6";
+import { FaHome } from "react-icons/fa";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import { MdRateReview } from "react-icons/md";
+import { TbBrandBooking } from "react-icons/tb";
+import { BiSolidFoodMenu } from "react-icons/bi";
+
+
+
+
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-  return ( 
+  return (
     <div className="flex">
-        {/* dashboard sidebar */}
-      <div className="w-64 min-h-full bg-orange-500">
+      {/* dashboard sidebar */}
+      <div className="w-64 min-h-screen bg-orange-500">
         <ul className="menu">
           <li>
-            <NavLink to="/dashboard/cart">My Cart</NavLink>
+            <NavLink to="/dashboard/userHome">
+              <FaHome />
+              User Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/reservation">
+              <FaRegCalendarAlt />
+              Reservation
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/cart">
+              <FaCartShopping />
+              My Cart
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/review">
+            <MdRateReview />
+             Add Review
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard/bookings">
+            <TbBrandBooking />
+
+             My Bookings
+            </NavLink>
+          </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome />
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/menu">
+            <BiSolidFoodMenu />
+
+              Menu
+            </NavLink>
           </li>
         </ul>
       </div>
-        {/* dashboard content */}
-      <div className="flex-1">
+      {/* dashboard content */}
+      <div className="flex-1 p-8">
         <Outlet></Outlet>
       </div>
     </div>
--- src/pages/Dashboard/Cart/Cart.jsx
+++ src/pages/Dashboard/Cart/Cart.jsx
@@ -1,10 +1,90 @@
+import useCart from "../../../Hooks/useCart";
+import { MdDeleteForever } from "react-icons/md";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 
 const Cart = () => {
-    return (
-        <div>
-            <h2> My Cart</h2>
-        </div>
-    );
+  const [cart,refetch] = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+  const axiosSecure = useAxiosSecure();
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/carts/${id}`).then((res) => {
+          console.log(res);
+          if (res.data.deletedCount > 0) {
+            refetch()
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your cart has been deleted.",
+                icon: "success"
+              });
+          }
+        });
+      }
+    });
+  };
+
+  return (
+    <div>
+      <div className="flex justify-evenly mb-7">
+        <h2 className="text-3xl">Total Items: {cart.length}</h2>
+        <h2 className="text-3xl">Total Price : {totalPrice}</h2>
+        <button className="btn btn-primary">Pay</button>
+      </div>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Image</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row 1 */}
+            {cart.map((item, index) => (
+              <tr key={item._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={item.image} alt="Item image" />
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>{item.name}</td>
+                <td>${item.price}</td>
+                <th>
+                  <button
+                    onClick={() => handleDelete(item._id)}
+                    className="btn btn-ghost btn-lg"
+                  >
+                    <RiDeleteBin6Line className="text-red-600" />
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
